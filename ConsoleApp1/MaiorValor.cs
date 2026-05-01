1a927b53a48b83a62712e6d9144981c6a93c0391
vitorribeiro3988-sys <vitorribeiro3988@gmail.com>
2026-04-30 21:17:26 -0300

    MaiorValor.cs

--- ConsoleApp1/MaiorValor.cs
+++ ConsoleApp1/MaiorValor.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,27 +11,16 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int n1;
-            int n2;
-
             Console.WriteLine("Digite o primeiro número:");
-            n1 = int.Parse(Console.ReadLine());
+
+            int n1 = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Digite o segundo número:");
-            n2 = int.Parse(Console.ReadLine());
 
-            if (n1 > n2)
-            {
-                Console.WriteLine("O primeiro número é maior");
-            }
+            int n2 = int.Parse(Console.ReadLine());
 
-            else
-
-            {
-                Console.WriteLine("O segundo número é maior");
-            }
-
-
+            if (n1 > n2) { Console.WriteLine("O primeiro número é maior!"); }
+            else { Console.WriteLine("O segundo número é maior"); }
         }
     }
 }
